acf326827b5041f664bea5c5e6254f4f3705b004
seanciaa <zenwisteria2003@gmail.com>
2024-11-02 18:10:05 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('FLOWER FOR CUTIEE CIAA').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
